407f11687519ccc405aa370a8f9e75d8102a100e
Jose Perez Rodriguez <joperezr@microsoft.com>
2022-07-08 14:54:34 -0700

    WIP

---
+++ src/benchmarks/micro/libraries/Common/SourceGeneratorRunner.cs
@@ -0,0 +1,90 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Text.RegularExpressions.Generator;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class SourceGeneratorRunner
+    {
+        internal static MetadataReference[] References { get; } = CreateReferences();
+
+        private static MetadataReference[] CreateReferences()
+        {
+            // Typically we'd want to use the right reference assemblies, but as we're not persisting any
+            // assets and only using this for testing purposes, referencing implementation assemblies is sufficient.
+            string corelibPath = typeof(object).Assembly.Location;
+            return new[]
+            {
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                    MetadataReference.CreateFromFile(Path.Combine(Path.GetDirectoryName(corelibPath), "System.Runtime.dll")),
+                    MetadataReference.CreateFromFile(typeof(Unsafe).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(Regex).Assembly.Location),
+                };
+        }
+
+        internal static async Task<IReadOnlyList<Diagnostic>> RunGenerator(
+                    string code, SourceGeneratorType sourceGeneratorType, bool compile = true, LanguageVersion langVersion = LanguageVersion.Preview, MetadataReference[] additionalRefs = null, bool allowUnsafe = false, CancellationToken cancellationToken = default)
+        {
+            var proj = new AdhocWorkspace()
+                .AddSolution(SolutionInfo.Create(SolutionId.CreateNewId(), VersionStamp.Create()))
+                .AddProject("SourceGeneratorTest", "SourceGeneratorTest.dll", "C#")
+                .WithMetadataReferences(additionalRefs != null ? References.Concat(additionalRefs) : References)
+                .WithCompilationOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: allowUnsafe)
+                .WithNullableContextOptions(NullableContextOptions.Enable))
+                .WithParseOptions(new CSharpParseOptions(langVersion))
+                .AddDocument("RegexGenerator.g.cs", SourceText.From(code, Encoding.UTF8)).Project;
+
+            proj.Solution.Workspace.TryApplyChanges(proj.Solution);
+
+            Compilation comp = await proj!.GetCompilationAsync(CancellationToken.None).ConfigureAwait(false);
+            Debug.Assert(comp != null);
+
+            IIncrementalGenerator generator = sourceGeneratorType switch
+            {
+                SourceGeneratorType.Regex => new RegexGenerator(),
+                _ => throw new ArgumentException($"Invalid {nameof(sourceGeneratorType)} value.")
+            };
+
+            CSharpGeneratorDriver cgd = CSharpGeneratorDriver.Create(new[] { generator.AsSourceGenerator() }, parseOptions: CSharpParseOptions.Default.WithLanguageVersion(langVersion));
+            GeneratorDriver gd = cgd.RunGenerators(comp!, cancellationToken);
+            GeneratorDriverRunResult generatorResults = gd.GetRunResult();
+            if (!compile)
+            {
+                return generatorResults.Diagnostics;
+            }
+
+            comp = comp.AddSyntaxTrees(generatorResults.GeneratedTrees.ToArray());
+            EmitResult results = comp.Emit(Stream.Null, cancellationToken: cancellationToken);
+            ImmutableArray<Diagnostic> generatorDiagnostics = generatorResults.Diagnostics.RemoveAll(d => d.Severity <= DiagnosticSeverity.Hidden);
+            ImmutableArray<Diagnostic> resultsDiagnostics = results.Diagnostics.RemoveAll(d => d.Severity <= DiagnosticSeverity.Hidden);
+            if (!results.Success || resultsDiagnostics.Length != 0 || generatorDiagnostics.Length != 0)
+            {
+                throw new ArgumentException(
+                    string.Join(Environment.NewLine, resultsDiagnostics.Concat(generatorDiagnostics)) + Environment.NewLine +
+                    string.Join(Environment.NewLine, generatorResults.GeneratedTrees.Select(t => t.ToString())));
+            }
+
+            return generatorResults.Diagnostics.Concat(results.Diagnostics).Where(d => d.Severity != DiagnosticSeverity.Hidden).ToArray();
+        }
+    }
+
+    public enum SourceGeneratorType
+    {
+        Regex
+    }
+}
---
+++ src/benchmarks/micro/libraries/System.Text.RegularExpressions/Perf.Regex.SourceGenerator.cs
@@ -0,0 +1,74 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Attributes;
+using MicroBenchmarks;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace System.Text.RegularExpressions.Tests
+{
+    [BenchmarkCategory(Categories.Libraries, Categories.Regex, Categories.NoWASM)]
+    public class Perf_Regex_SourceGenerator
+    {
+        public enum BenchmarkType
+        {
+            UsesGenerator,
+            DoesNotUseGenerator
+        }
+
+        [Params(BenchmarkType.UsesGenerator, BenchmarkType.DoesNotUseGenerator)]
+        public BenchmarkType TestType { get; set; }
+
+        private string _code;
+
+        private const string UsesGeneratorCodeTemplate = @"
+public partial class Class#i#
+{
+    [RegexGenerator(""(a|b)#i#"", RegexOptions.IgnoreCase)]
+    private static partial Regex MyRegex();
+}";
+
+        private const string DoesNotUseGeneratorCodeTemplate = @"
+public partial class Class#i#
+{
+    [MyCustom(""(a|b)#i#"", RegexOptions.IgnoreCase)]
+    private static Regex MyRegex() => throw null;
+}";
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            if (TestType == BenchmarkType.UsesGenerator)
+            {
+                StringBuilder sb = new StringBuilder("using System.Text.RegularExpressions;");
+                for (int i = 0; i < 100; i++)
+                {
+                    sb.Append(UsesGeneratorCodeTemplate.Replace("#i#", i.ToString()));
+                }
+                _code = sb.ToString();
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder(@"using System.Text.RegularExpressions;
+using System;
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public sealed class MyCustomAttribute : Attribute
+{
+    public MyCustomAttribute(string pattern, RegexOptions options)
+    { }
+}");
+                for (int i = 0; i < 100; i++)
+                {
+                    sb.Append(DoesNotUseGeneratorCodeTemplate.Replace("#i#", i.ToString()));
+                }
+                _code = sb.ToString();
+            }
+        }
+
+        [Benchmark]
+        public async Task RunRegexGenerator()
+            => await SourceGeneratorRunner.RunGenerator(_code, SourceGeneratorType.Regex);
+    }
+}
